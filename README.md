100c342ee00f56a7e442f514f3fc1004b6aafb86
Alexandre BARDAN <bardan.alexandre@gmail.com>
2019-06-28 17:41:18 +0200

    update the readme && add some comments

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # sport-players
+
+A super project !
+
+## Start the server
+
+1. Move in the project directory
+
+2. Install all dependencies with npm
+    - Using [npm](https://www.npmjs.com/#getting-started): `npm i`
+
+3. Launch the server
+    - Using [npm](https://www.npmjs.com/#getting-started): `npm start`
+    - Or you can launch the index file in the src folder
+
+## Test
+
+1. Move in the project directory
+
+2. Install all devv dependencies with npm (check your NODE_ENV is set with 'production')
+    - Using [npm](https://www.npmjs.com/#getting-started): `npm i`
+
+3. Launch the test
+    - Using [npm](https://www.npmjs.com/#getting-started): `npm test`
--- src/routes/players.js
+++ src/routes/players.js
@@ -7,12 +7,15 @@ const route = new Router({
 
 const JSON_PATH = '../headtohead.json'
 
+// return a list of players
 route.get('/', ctx => {
     const { players } = require(JSON_PATH)
+    // ASC sorting players by id
     if (players) players.sort((a, b) => a.id > b.id)
     ctx.body = players
 })
 
+// return a player or return a 404 error status if not exist
 route.get('/:id', ctx => {
     const { id } = ctx.params
     const data = require(JSON_PATH)
@@ -22,6 +25,7 @@ route.get('/:id', ctx => {
     }
 })
 
+// delete a player or return a 404 error status if the id user not exist
 route.del('/:id', ctx => {
     const { id } = ctx.params
     const data = require(JSON_PATH)
